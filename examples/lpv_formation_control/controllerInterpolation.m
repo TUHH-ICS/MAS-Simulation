cb4e96baa411200e6906a4759da6a961738c7d91
Christian Hespe <christian.hespe@tuhh.de>
2020-07-13 15:14:19 +0200

    Remove unnecessary file

--- examples/lpv_formation_control/controllerInterpolation.m
+++
@@ -1,7 +0,0 @@
-function K = controllerInterpolation(rho, bounds, Ks)
-%CONTROLLERINTERPOLATION Performs a linear interpolation between two
-%controller matrices that are given by Ks.
-scaled = (rho - bounds(1))/(bounds(2) - bounds(1));
-K      = (1 - scaled) * Ks{1} + scaled * Ks{2};
-end
-
